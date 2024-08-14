7e2d33c49d3df84bc552aa9c038fc481fa94db9f
haeun1107 <haeun9394@gmail.com>
2024-08-14 22:55:37 +0900

    🎉 Init: 환경변수 설정

---
+++ src/main/java/farm/rangers/RangersApplication.java
@@ -0,0 +1,13 @@
+package farm.rangers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/farm/rangers/RangersApplicationTests.java
@@ -0,0 +1,13 @@
+package farm.rangers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
