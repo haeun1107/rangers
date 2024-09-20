67990a4c22ca3774b774e6719141f035a1b1eba9
haeun1107 <haeun9394@gmail.com>
2024-09-20 16:08:30 +0900

    🐛 Fix: health check url

--- src/main/java/farm/rangers/global/RootController.java
+++ src/main/java/farm/rangers/global/RootController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public String hc() {
         return "success!";
     }
